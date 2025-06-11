2cd7280776ff33d678ce72640563a68784b856f9
maria-hbp <maria.guarda@alumnos.ucn.cl>
2025-06-10 20:02:21 -0400

    First commit

---
+++ config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  development: {
+    dialect: 'sqlite',
+    storage: './database.sqlite',
+    logging: false
+  },
+  test: {
+    dialect: 'sqlite',
+    storage: ':memory:'
+  },
+  production: {
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+  }
+};
